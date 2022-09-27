adfdafff31e33473cfe4a45368caafdfc43ccbe2
HobotM <mateuszhobot@gmail.com>
2022-09-27 21:18:27 +0100

    css datting cards

--- src/components/DatingCard.js
+++ src/components/DatingCard.js
@@ -6,28 +6,24 @@ const DatingCards = () => {
   const [people, setPeople] = useState([
     {
       name: "Random Guy",
-      imgUrl:
-        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      imgUrl: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixid=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80",
     },
 
     {
       name: "Another Guy",
-      imgUrl:
-        "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+      imgUrl: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
     },
 
     {
       name: "Random Girl",
-      imgUrl:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+      imgUrl: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
     },
 
     {
       name: "Another Girl",
-      imgUrl:
-        "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+      imgUrl: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
     },
-  ]);
+  ])
 
   const swiped = (direction, nameToDelete) => {
     console.log("receiving" + nameToDelete);
@@ -47,10 +43,7 @@ const DatingCards = () => {
             onSwipe={(dir) => swiped(dir, person.name)}
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
-            <div
-              style={{ backgroundImage: "url(${person.imgUrl})" }}
-              className="card"
-            >
+            <div style={{ backgroundImage: `url(${person.imgUrl})` }} className="card">
               <h3>{person.name}</h3>
             </div>
           </DatingCard>
