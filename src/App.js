b9927b23a0a406fe716246619d91e54b258f106c
HobotM <mateuszhobot@gmail.com>
2022-09-27 18:24:39 +0100

    datin-app headers added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import DatingCard from './components/DatingCard';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Dating app mern</h1>
+     <Header />
+     <DatingCard />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@mui/icons-material/Person'
+import IconButton from '@mui/material/IconButton'
+import ForumIcon from '@mui/icons-material/Forum'
+
+const Header = () => {
+
+    return( 
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header_icon" />
+            </IconButton>
+            <img className="header_logo" src="logo192.png" alt="header" />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header_icon" />
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
